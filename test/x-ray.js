bfea5f040f121601537dc4ca1ca9dc25735109d1
Matthew Mueller <mattmuelle@gmail.com>
2015-02-04 02:43:02 -0800

    add xray.format(fn)

--- test/x-ray.js
+++ test/x-ray.js
@@ -5,6 +5,7 @@
 var rm = require('fs').unlinkSync;
 var join = require('path').join;
 var assert = require('assert');
+var subs = require('subs');
 var xray = require('..');
 var fs = require('fs');
 
@@ -14,95 +15,109 @@ var fs = require('fs');
 
 describe('x-ray', function() {
 
-  describe('http', function() {
-
-    it('should select keys', function(done) {
-      var fixture = get('select-keys');
-      xray('http://mat.io')
-        .select(fixture.input)
-        .run(function(err, arr) {
-          if (err) return done(err);
-          assert.deepEqual(arr.pop(), fixture.expected);
-          done();
-        });
-    });
-
-    it('should stream to a file', function(done) {
-      var fixture = get('select-keys');
-      var path = join(__dirname, 'out.json');
-
-      xray('http://mat.io')
-        .select(fixture.input)
-        .write(path)
-        .on('error', done)
-        .on('close', function() {
-          var str = fs.readFileSync(path, 'utf8');
-          var arr = JSON.parse(str);
-          assert.deepEqual(arr.pop(), fixture.expected);
-          rm(path);
-          done();
-        });
-    })
-
-    it('should paginate', function(done) {
-      var fixture = get('paginate');
-      xray('https://github.com/stars/matthewmueller')
-        .select(fixture.input)
-        .paginate('.pagination a:last-child[href]')
-        .limit(2)
-        .run(function(err, arr) {
-          if (err) return done(err);
-          fixture.expected(arr);
-          done();
-        });
-    });
-
-    it('should add delay to pagination', function(done) {
-      var fixture = get('paginate');
-      xray('https://github.com/stars/matthewmueller')
-        .select(fixture.input)
-        .paginate('.pagination a:last-child[href]')
-        .limit(2)
-        .delay(2000)
-        .run(function(err, arr) {
-          if (err) return done(err);
-          fixture.expected(arr);
-          done();
-        });
-    })
-
-    it('should stream to a file and paginate', function(done) {
-      var fixture = get('paginate');
-      var path = join(__dirname, 'out.json');
-
-      xray('https://github.com/stars/matthewmueller')
-        .select(fixture.input)
-        .paginate('.pagination a:last-child[href]')
-        .limit(2)
-        .write(path)
-        .on('error', done)
-        .on('close', function() {
-          var str = fs.readFileSync(path, 'utf8');
-          var arr = JSON.parse(str);
-          fixture.expected(arr);
-          rm(path);
-          done();
-        });
-    });
-
-    it('should yield an empty array on unmatched collections', function(done) {
-      xray('http://mat.io')
-        .select([{
-          title: '.titlez'
-        }])
-        .run(function(err, arr) {
-          if (err) return done(err);
-          assert.deepEqual([], arr);
-          done();
-        })
-    })
+  it('should select keys', function(done) {
+    var fixture = get('select-keys');
+    xray('http://mat.io')
+      .select(fixture.input)
+      .run(function(err, arr) {
+        if (err) return done(err);
+        assert.deepEqual(arr.pop(), fixture.expected);
+        done();
+      });
   });
 
+  it('should stream to a file', function(done) {
+    var fixture = get('select-keys');
+    var path = join(__dirname, 'out.json');
+
+    xray('http://mat.io')
+      .select(fixture.input)
+      .write(path)
+      .on('error', done)
+      .on('close', function() {
+        var str = fs.readFileSync(path, 'utf8');
+        var arr = JSON.parse(str);
+        assert.deepEqual(arr.pop(), fixture.expected);
+        rm(path);
+        done();
+      });
+  })
+
+  it('should paginate', function(done) {
+    var fixture = get('paginate');
+    xray('https://github.com/stars/matthewmueller')
+      .select(fixture.input)
+      .paginate('.pagination a:last-child[href]')
+      .limit(2)
+      .run(function(err, arr) {
+        if (err) return done(err);
+        fixture.expected(arr);
+        done();
+      });
+  });
+
+  it('should add delay to pagination', function(done) {
+    var fixture = get('paginate');
+    xray('https://github.com/stars/matthewmueller')
+      .select(fixture.input)
+      .paginate('.pagination a:last-child[href]')
+      .limit(2)
+      .delay(2000)
+      .run(function(err, arr) {
+        if (err) return done(err);
+        fixture.expected(arr);
+        done();
+      });
+  })
+
+  it('should stream to a file and paginate', function(done) {
+    var fixture = get('paginate');
+    var path = join(__dirname, 'out.json');
+
+    xray('https://github.com/stars/matthewmueller')
+      .select(fixture.input)
+      .paginate('.pagination a:last-child[href]')
+      .limit(2)
+      .write(path)
+      .on('error', done)
+      .on('close', function() {
+        var str = fs.readFileSync(path, 'utf8');
+        var arr = JSON.parse(str);
+        fixture.expected(arr);
+        rm(path);
+        done();
+      });
+  });
+
+  it('should yield an empty array on unmatched collections', function(done) {
+    xray('http://mat.io')
+      .select([{
+        title: '.titlez'
+      }])
+      .run(function(err, arr) {
+        if (err) return done(err);
+        assert.deepEqual([], arr);
+        done();
+      })
+  })
+
+  it('should support formatting', function(done) {
+    var fixture = get('select-keys');
+    xray('http://mat.io')
+      .select(fixture.input)
+      .format(format)
+      .run(function(err, arr) {
+        if (err) return done(err);
+        arr.forEach(function(item) {
+          assert(~item.indexOf('<a href="http'));
+        })
+        done();
+      })
+
+    function format(obj) {
+      return subs('<a href="{link}">{thumb}</a>', obj);
+    }
+  })
 })
 
 /**
