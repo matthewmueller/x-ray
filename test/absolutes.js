11dd8f7ecdd72961b3ceccb5f6c4c24f0e3488f6
Nerijus Narmontas <n.narmontas@gmail.com>
2015-12-28 18:22:40 +0200

    Added support for <base> tag when generating absolute URLs.

--- test/absolutes.js
+++ test/absolutes.js
@@ -24,3 +24,18 @@ describe('absolute URLs', function(){
     assert.equal('<a href="http://example.com/bar.html"></a>', absolute(path, $el).html());
   });
 });
+
+describe('absolute URLs with <base> tag', function () {
+  var head = '<head><base href="http://example.com/foo/"></head>';
+  var path = 'http://example.com/foo.html';
+   
+  it('should convert relative URL', function () {
+    var $el = cheerio.load(head + '<a href="foobar.html"></a>');
+    assert.equal(head + '<a href="http://example.com/foo/foobar.html"></a>', absolute(path, $el).html());
+  });
+   
+  it('should not convert relative URL starting with /', function () {
+    var $el = cheerio.load(head + '<a href="/foobar.html"></a>');
+    assert.equal(head + '<a href="http://example.com/foobar.html"></a>', absolute(path, $el).html());
+  });
+});
