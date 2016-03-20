a26f8e54ace2de4e17fe6a3fbfd610d30785ef6c
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-03-20 23:07:02 +0100

    Disable security in testing

--- test/xray_driver_spec.js
+++ test/xray_driver_spec.js
@@ -9,7 +9,9 @@ var assert = require('assert')
 describe('Xray.driver(fn)', function () {
   it('should support basic phantom', function (done) {
     var x = Xray()
-      .driver(phantom())
+      .driver(phantom({
+        webSecurity: false
+      }))
 
     x('http://www.google.com/ncr', 'title')(function (err, str) {
       if (err) return done(err)
