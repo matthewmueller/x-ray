b6e974089888471440a826c3b36ce2bad09042f3
Anonymous <anonymous@test.com>
2019-07-14 04:52:56 +0800

    add test for #350

--- test/xray_spec.js
+++ test/xray_spec.js
@@ -372,6 +372,16 @@ describe('Xray basics', function () {
     })
   })
 
+  it('it should not decode non-latin HTML automatically when using `@html` selector', function (done) {
+    var x = Xray()
+
+    x('<div>你好</div>', 'div@html')(function (err, result) {
+      assert.ifError(err)
+      assert.equal(result, '你好')
+      done()
+    })
+  })
+
   describe('.format()', function () {
     xit('should support adding formatters', function () {
       // TODO
